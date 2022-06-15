58b3487826ab762304c913a3b29f62dc85094972
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-15 16:45:23 -0300

    Cria a classe abstrata Conversao e a classe ConversaoDolar

---
+++ Aula11/ConversaoDolar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11
+{
+    public class ConversaoDolar : Conversao
+    {
+
+        private double cotacaoDaMoedaHoje;
+        private double valorDaCompraEmReais;
+        private double valorConvertido;
+        public double CotacaoDaMoedaHoje
+        {
+            get
+            {
+                return cotacaoDaMoedaHoje;
+            }
+            set
+            {
+                cotacaoDaMoedaHoje = value;
+            }
+        }
+
+        /*
+        public override double cotacaoDaMoedaHoje 
+        {
+            get; set;
+        }
+        */
+
+        public override void ConversaoMaxima()
+        {
+
+        }
+        public override void ConversaoMinima()
+        {
+
+        }
+        public override void ConversaoCompra()
+        {
+            valorConvertido = valorDaCompraEmReais/cotacaoDaMoedaHoje;
+        }
+    }
+}
